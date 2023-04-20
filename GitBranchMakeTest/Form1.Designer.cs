58f065bbd795ade2e96f8d4b94dddf2db33c9926
iguchimalcom <103982549+iguchimalcom@users.noreply.github.com>
2023-04-20 13:13:36 +0900

    Ver1.10 fix.

--- GitBranchMakeTest/Form1.Designer.cs
+++ GitBranchMakeTest/Form1.Designer.cs
@@ -41,7 +41,7 @@
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(43, 12);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Ver1.00";
+            this.label1.Text = "Ver1.10";
             // 
             // label2
             // 
